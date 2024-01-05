9f60862dbcd91f58698e0574d6d9cbb732d30917
Lucas Colombo <lucasncolombo@gmail.com>
2024-01-05 16:52:51 -0300

    feat: ✨ seo metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'kurv';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'kurv » lightweight process manager';
+export const SITE_DESCRIPTION = 'kurv is a lightweight process manager and daemonizer for windows and linux, written in rust.';
